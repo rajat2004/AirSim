de81a424bdc29226db19c472c759f817e9095fed
Rajat Singhal <rajatsinghal564@gmail.com>
2019-08-15 08:33:09 +0530

    Chnages for data collection

--- PythonClient/multirotor/opencv_show.py
+++ PythonClient/multirotor/opencv_show.py
@@ -35,9 +35,9 @@ print (cameraTypeMap[cameraType])
 
 client = airsim.MultirotorClient()
 client.confirmConnection()
-client.enableApiControl(True)
-client.armDisarm(True)
-client.takeoffAsync().join()
+# client.enableApiControl(True)
+# client.armDisarm(True)
+# client.takeoffAsync().join()
 
 help = False
 
@@ -51,15 +51,29 @@ frameCount = 0
 startTime=time.clock()
 fps = 0
 
+rawImage = client.simGetImage("3", cameraTypeMap[cameraType])
+png = cv2.imdecode(airsim.string_to_uint8_array(rawImage), cv2.IMREAD_UNCHANGED)
+height, width, channels = png.shape
+print(height)
+print(width)
+print(channels)
+
+# Define the codec and create VideoWriter object
+fourcc = cv2.VideoWriter_fourcc('M','P','E','G') # Be sure to use lower case
+out = cv2.VideoWriter("try1.avi", fourcc, 20.0, (width, height), True)
+
 while True:
     # because this method returns std::vector<uint8>, msgpack decides to encode it as a string unfortunately.
-    rawImage = client.simGetImage("0", cameraTypeMap[cameraType])
+    rawImage = client.simGetImage("3", cameraTypeMap[cameraType])
     if (rawImage == None):
         print("Camera is not returning image, please check airsim for error messages")
         sys.exit(0)
     else:
         png = cv2.imdecode(airsim.string_to_uint8_array(rawImage), cv2.IMREAD_UNCHANGED)
-        cv2.putText(png,'FPS ' + str(fps),textOrg, fontFace, fontScale,(255,0,255),thickness)
+        # cv2.putText(png,'FPS ' + str(fps),textOrg, fontFace, fontScale,(255,0,255),thickness)
+        # height, width, channels = png.shape
+        cv2.cvtColor(png,cv2.COLOR_RGB2BGR)
+        out.write(png)
         cv2.imshow("Depth", png)
 
     frameCount  = frameCount  + 1
@@ -72,4 +86,8 @@ while True:
     
     key = cv2.waitKey(1) & 0xFF;
     if (key == 27 or key == ord('q') or key == ord('x')):
-        break;
+        break
+
+print("Release")
+cv2.destroyAllWindows()
+out.release()
