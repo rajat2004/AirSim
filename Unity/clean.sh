59fe8edd6dc262a940290a59d1456aa8107d3efd
Rajat Singhal <rajatsinghal564@gmail.com>
2019-11-27 20:59:45 +0530

    Add clean.sh for Unity

---
+++ Unity/clean.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+# get path of current script: https://stackoverflow.com/a/39340259/207661
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+pushd "$SCRIPT_DIR" >/dev/null
+
+set -e
+set +x
+
+rm -rf linux-build
+rm -rf UnityDemo/Assets/Plugins
+
+popd >/dev/null
