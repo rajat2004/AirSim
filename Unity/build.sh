44977d9554a1979ac492c5cc96400c50d8d28f31
Rajat Singhal <rajatsinghal564@gmail.com>
2019-11-27 21:00:49 +0530

    Fix Unity build.sh
    Fix CMake detection, non-standard string comparision, remove whitespace, spurious semicolons
    Add debug info, exit on failure

--- Unity/build.sh
+++ Unity/build.sh
@@ -1,14 +1,21 @@
+#!/bin/bash
+
+set -x
+set -e
+
+# check for rpclib
 if [ ! -f ../external/rpclib/rpclib-2.2.1/rpclib.pc.in ]; then
-    >&2 echo "error, rpc.pc.in not found"
+    >&2 echo "error, rpc.pc.in not found, please run setup.sh first and then run build.sh again"
 fi
+
 cp ../external/rpclib/rpclib-2.2.1/rpclib.pc.in AirLibWrapper/AirsimWrapper/rpclib.pc.in
 
 if [ ! -d "linux-build" ]; then
-    mkdir linux-build;
+    mkdir linux-build
 fi
 
 
-cd linux-build;
+cd linux-build
 if [[ -n $CIINSTALL ]]; then # use downloaded binaries on Travis
     export CC=${LLVM_DIR}/bin/clang
     export CXX=${LLVM_DIR}/bin/clang++
@@ -17,25 +24,30 @@ else
     export CXX="clang++-5.0"
 fi
 
-if [ "$(uname)" == "Darwin" ]; then
-    CMAKE="$(greadlink -f ../../cmake_build/bin/cmake)"
+# check for local cmake build created by setup.sh
+if [ -d "../../cmake_build" ]; then
+    if [ "$(uname)" = "Darwin" ]; then
+        CMAKE="$(greadlink -f ../../cmake_build/bin/cmake)"
+    else
+        CMAKE="$(readlink -f ../../cmake_build/bin/cmake)"
+    fi
 else
-    CMAKE="$(readlink -f ../../cmake_build/bin/cmake)"
+    CMAKE=$(which cmake)
 fi
 
-"$CMAKE" ../../cmake ../AirLibWrapper/AirsimWrapper;
-make -j`nproc`;
+"$CMAKE" ../../cmake ../AirLibWrapper/AirsimWrapper
+make -j`nproc`
 if [ ! -d "../UnityDemo/Assets/Plugins" ]; then
-	mkdir ../ UnityDemo/Assets/Plugins;
+    mkdir ../UnityDemo/Assets/Plugins
 fi
 
 if [ "$(uname)" == "Darwin" ]; then
-	cp -r AirsimWrapper.bundle ../UnityDemo/Assets/Plugins;
+    cp -r AirsimWrapper.bundle ../UnityDemo/Assets/Plugins
 else
-	cp libAirsimWrapper.so ../UnityDemo/Assets/Plugins;
+    cp libAirsimWrapper.so ../UnityDemo/Assets/Plugins
 fi
 
 cd ..
 if [ -f AirLibWrapper/AirsimWrapper/rpclib.pc.in ]; then
-	rm AirLibWrapper/AirsimWrapper/rpclib.pc.in
+    rm AirLibWrapper/AirsimWrapper/rpclib.pc.in
 fi
