e15ab55a2f842007ccf6f7a0f9adc4aae3b3ae50
Rajat Singhal <rajatsinghal564@gmail.com>
2020-01-13 21:48:05 +0530

    Fix finding CMake binary in OSX

--- setup.sh
+++ setup.sh
@@ -100,39 +100,39 @@ else #linux
     fi
 
     #install additional tools
-    sudo apt-get install -y build-essential
+    sudo apt-get install -y build-essential cmake
     sudo apt-get install -y unzip
+fi
 
-    cmake_ver=$(cmake --version 2>&1 | head -n1 | cut -d ' ' -f3 | awk '{print $NF}')
-    cmake_path=$(which cmake)
-    if [[ "$cmake_path" == "" ]] ; then
-        cmake_ver=0
+cmake_ver=$(cmake --version 2>&1 | head -n1 | cut -d ' ' -f3 | awk '{print $NF}')
+cmake_path=$(which cmake)
+if [[ "$cmake_path" == "" ]] ; then
+    cmake_ver=0
+fi
+
+#download cmake - v3.10.2 is not out of box in Ubuntu 16.04
+if version_less_than_equal_to $cmake_ver $MIN_CMAKE_VERSION; then
+    if [[ ! -d "cmake_build/bin" ]]; then
+        echo "Downloading cmake..."
+        wget https://cmake.org/files/v3.10/cmake-3.10.2.tar.gz \
+            -O cmake.tar.gz
+        tar -xzf cmake.tar.gz
+        rm cmake.tar.gz
+        rm -rf ./cmake_build
+        mv ./cmake-3.10.2 ./cmake_build
+        pushd cmake_build
+        ./bootstrap
+        make
+        popd
     fi
-
-    #download cmake - v3.10.2 is not out of box in Ubuntu 16.04
-    if version_less_than_equal_to $cmake_ver $MIN_CMAKE_VERSION; then
-        if [[ ! -d "cmake_build/bin" ]]; then
-            echo "Downloading cmake..."
-            wget https://cmake.org/files/v3.10/cmake-3.10.2.tar.gz \
-                -O cmake.tar.gz
-            tar -xzf cmake.tar.gz
-            rm cmake.tar.gz
-            rm -rf ./cmake_build
-            mv ./cmake-3.10.2 ./cmake_build
-            pushd cmake_build
-            ./bootstrap
-            make
-            popd
-        fi
-        if [ "$(uname)" == "Darwin" ]; then
-            CMAKE="$(greadlink -f cmake_build/bin/cmake)"
-        else
-            CMAKE="$(readlink -f cmake_build/bin/cmake)"
-        fi
+    if [ "$(uname)" == "Darwin" ]; then
+        CMAKE="$(greadlink -f cmake_build/bin/cmake)"
     else
-        echo "Already have good version of cmake: $cmake_ver"
-        CMAKE=$(which cmake)
+        CMAKE="$(readlink -f cmake_build/bin/cmake)"
     fi
+else
+    echo "Already have good version of cmake: $cmake_ver"
+    CMAKE=$(which cmake)
 fi
 
 # Download rpclib
